00af7891feed7165bd7a2d881c93f6713b334556
nmarrocchi <nmarrocchi@la-providence.net>
2021-09-21 09:50:20 +0200

    function afficheCoords

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ function check() {
 function DeleteAccount(){
     if(isset($_POST["Delete_Account"])){
         $_SESSION["Logged"] = false;
-        $Account_Delete = $BDD->query("DELETE FROM `user` WHERE id = '".$_SESSION["ID_User"]."'");
+        $Account_Delete = $BDD->query("DELETE FROM `user` WHERE id = '".$_SESSION["ID_User"]."'")THEN CAST(1 AS BIT)ELSE CAST(0 AS BIT) END;
         header("location: index.php");
     }
 }
