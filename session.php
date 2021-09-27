ae95a395b80ab07ed4e4ce5530a5d4b162300545
CAUET-Clement <ccauet@la-providence.net>
2021-09-27 15:41:12 +0200

    Correstion du systeme de connexion

--- session.php
+++ session.php
@@ -1,16 +1,22 @@
 <?php
-    session_start();
+    include "src/class/user.php";
+
+    /*$host = "127.0.0.1";
+    $dbname = "tp2";
+    $login = "root";
+    $mdp = "";*/
+
     // - Gestion de la bdd
     $host = "192.168.64.204";
     $dbname = "tp1";
-    $login = "root";
-    $mdp = "";
+    $login = "admin";
+    $mdp = "admin";
 
     $bdd = new PDO('mysql:host='.$host.'; dbname='.$dbname.'; charset=utf8', $login, $mdp);
 
     $user = new user($bdd);
 
-    $fonction = new fonction($bdd);
+    //$fonction = new fonction($bdd);
 
     if (isset($_SESSION["Connected"]) && $_SESSION["Connected"] == true){
         if(isset($_SESSION["userID"])){
